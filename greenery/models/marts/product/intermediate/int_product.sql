e0fc934cabe2972297d36bd4a1f652303f9a8dca
Jacob Concha <jconcha56429@gmail.com>
2022-06-19 07:04:21 +0000

    models almost done

---
+++ greenery/models/marts/product/intermediate/int_product.sql
@@ -0,0 +1,23 @@
+{{
+    config(
+        MATERIALIZED='view'
+    )
+}}
+
+SELECT 
+    e.user_id,
+    a.address,
+    a.zipcode,
+    a.state,
+    a.country,
+    e.event_id,
+    e.session_id,
+    e.created_at AS event_created_at,
+    e.event_type,
+    e.order_id,
+    e.product_id
+FROM {{ ref('greenery_events')}} e
+LEFT JOIN {{ref('greenery_orders')}} o
+ON o.order_id = e.order_id 
+LEFT JOIN {{ref('greenery_addresses')}} a
+ON a.address_id = o.address_id
